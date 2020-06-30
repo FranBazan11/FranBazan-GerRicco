31baffeaf4d7cb2935a4205a3254e6913715eb8b
German Ricco <germanricco97@gmail.com>
2020-06-30 16:48:47 -0300

    Agregamos archivo de experimento

---
+++ Experimento_PromedioDeNotas.py
@@ -0,0 +1,13 @@
+print("Programa de libreta")
+notas_matematica=int(input("Cuantas notas tenes en matematicas? "))
+matematicas=[]
+suma=0
+
+while len(matematicas) < notas_matematica:
+	matematicas.insert(int(len(matematicas)),input("inserte la "+ str(1+int(len(matematicas))) + " nota de matematica: "))
+
+for i in range(int(len(matematicas))):
+	suma = suma + int(matematicas[i])
+
+promedio= round(suma/len(matematicas),2)
+print("tu promedio en matematicas es: " + str(promedio))
